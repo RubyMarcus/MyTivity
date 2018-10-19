85435949bce6857532d650fce3d0c489112f1830
Robin Tapper <robintapper4@gmail.com>
2018-10-19 13:40:03 +0200

    added splashscreen, icons, background

---
+++ app/src/main/java/com/apia22018/sportactivities/screens/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.apia22018.sportactivities.screens.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
