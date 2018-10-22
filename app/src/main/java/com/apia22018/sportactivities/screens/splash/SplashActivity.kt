494dbce14815074b764fa7471786102f1c18fd52
Robin Tapper <robintapper4@gmail.com>
2018-10-22 13:36:26 +0200

    login view och activity

--- app/src/main/java/com/apia22018/sportactivities/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/apia22018/sportactivities/screens/splash/SplashActivity.kt
@@ -2,10 +2,14 @@ package com.apia22018.sportactivities.screens.splash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.apia22018.sportactivities.screens.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        LoginActivity.start(this)
+        finish()
     }
 }
